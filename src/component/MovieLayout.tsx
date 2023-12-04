c89e2a58e0d24c9e5a74a10aa85937c3944d9c1e
kanghunK <ltbehappy16@gmail.com>
2023-12-04 17:08:00 +0900

    Rename: AppLayout.tsx -> MovieLayout.tsx 변경 Create: Movie 페이지

--- src/component/MovieLayout.tsx
+++ src/component/MovieLayout.tsx
@@ -5,7 +5,7 @@ import { StrictPropsWithChildren } from "../type/types";
 import RightIcon from "../assets/rightArrow.svg?react";
 import LeftMenuContext from "../context/LeftMenuContext";
 
-function AppLayout({ children }: StrictPropsWithChildren) {
+function MovieLayout({ children }: StrictPropsWithChildren) {
     const [leftMenuState, setLeftMenuState] = useState(false);
     const leftMenuContextValue = useMemo(
         () => ({ leftMenuState, setLeftMenuState }),
@@ -18,9 +18,14 @@ function AppLayout({ children }: StrictPropsWithChildren) {
                 <LeftMenu />
                 <RootContainer>
                     <Header>
-                        <h1>CineInsight</h1>
-                        <p>영화 정보 조회 사이트</p>
-                        <div onClick={() => setLeftMenuState(true)}>
+                        <div className="title">
+                            <h1>CineInsight</h1>
+                            <span>영화 정보 조회 사이트</span>
+                        </div>
+                        <div
+                            className="menu_btn"
+                            onClick={() => setLeftMenuState(true)}
+                        >
                             <RightIcon />
                         </div>
                     </Header>
@@ -31,10 +36,26 @@ function AppLayout({ children }: StrictPropsWithChildren) {
     );
 }
 
-export default AppLayout;
+export default MovieLayout;
 
 const RootContainer = styled.div``;
 
-const Header = styled.header``;
+const Header = styled.header`
+    position: relative;
+
+    .title {
+        overflow: hidden;
+        text-align: center;
+    }
+
+    .menu_btn {
+        position: absolute;
+        top: 20px;
+        left: 20px;
+        display: inline-block;
+
+        cursor: pointer;
+    }
+`;
 
 const MainContainer = styled.main``;
