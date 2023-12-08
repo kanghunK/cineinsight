6aa6c9c071fd5e236199bd32fa3f0ee7559de442
kanghunK <ltbehappy16@gmail.com>
2023-12-08 22:57:48 +0900

    Add: redux store 파일

---
+++ src/app/store.ts
@@ -0,0 +1,21 @@
+import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+    reducer: {
+        // [movieApi.reducerPath]: movieApi.reducer,
+    },
+    // middleware: (getDefaultMiddleware) =>
+    //     getDefaultMiddleware().concat(movieApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>;
