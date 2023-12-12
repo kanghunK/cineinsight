7de871257e238ab9bf6c824cb07b1c2692606a33
kanghunK <ltbehappy16@gmail.com>
2023-12-12 12:17:16 +0900

    무비 장르 데이터 받아오는 훅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
+import { useGetMovieGenreDataQuery } from "./app/services/movieAPI";
 
 function App() {
+    const { error, isLoading } = useGetMovieGenreDataQuery();
+
     const router = createBrowserRouter([
         {
             path: "/",
@@ -13,6 +16,10 @@ function App() {
             element: <Movie />,
         },
     ]);
+
+    if (isLoading && !error) return <div>로딩중...</div>;
+
+    if (error) return <div>데이터 서버에 연결하지 못하였습니다..</div>;
 
     return <RouterProvider router={router} />;
 }
