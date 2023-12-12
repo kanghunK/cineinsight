912179ac2c5d4219cd8741d76828e2866f1e7a5f
kanghunK <ltbehappy16@gmail.com>
2023-12-12 17:05:06 +0900

    Add: minireset.css

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import { store } from "./app/store.ts";
 import "./index.css";
+import "minireset.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
